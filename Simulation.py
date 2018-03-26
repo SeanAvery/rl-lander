a9edee1977eeb29ee1100c98cd0326e06ed1ff4e
Sean Pollock <seanpollock3344@gmail.com>
2018-03-26 16:57:08 -0400

    simulation with random actions taken

--- Simulation.py
+++ Simulation.py
@@ -1,13 +1,12 @@
 import gym
+import random
+from time import sleep
 
 class Simulation():
     def __init__(self):
         self.env = gym.make('LunarLander-v2')
         self.get_action_dim()
         self.get_space_dim()
-        print('action dim len', self.action_dim_len)
-        print('state_dim_len', self.state_dim_len)
-        print('sta_dim_low', self.state_dim_low)
 
     def get_action_dim(self):
         self.action_dim_len =self.env.action_space.n
@@ -16,7 +15,22 @@ class Simulation():
         self.state_dim_len = len(self.env.observation_space.low)
         self.state_dim_low = self.env.observation_space.low
         self.state_dim_high = self.env.observation_space.high
-
+    
+    def choose_action(self):
+        print('action_dim_len', self.action_dim_len)
+        return random.randint(0, self.action_dim_len - 1)
+    
+    def run(self, num_episodes):
+        for i in range(num_episodes):
+            self.env.reset()
+            while True:
+                self.env.render()
+                action = self.choose_action()
+                state, reward, done, info = self.env.step(action)
+                sleep(0.1)
+                if done:
+                    break
+        
 if __name__ == '__main__':
     simulation = Simulation()
-
+    simulation.run(3)
