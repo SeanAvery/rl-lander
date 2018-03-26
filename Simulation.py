b4880d10057995cde3132050066acdd505a2809e
Sean Pollock <seanpollock3344@gmail.com>
2018-03-26 16:30:48 -0400

    basic state and action sapce lookups

--- Simulation.py
+++ Simulation.py
@@ -3,7 +3,19 @@ import gym
 class Simulation():
     def __init__(self):
         self.env = gym.make('LunarLander-v2')
-        print('self.env', self.env)
+        self.get_action_dim()
+        self.get_space_dim()
+        print('action dim len', self.action_dim_len)
+        print('state_dim_len', self.state_dim_len)
+        print('sta_dim_low', self.state_dim_low)
+
+    def get_action_dim(self):
+        self.action_dim_len =self.env.action_space.n
+
+    def get_space_dim(self):
+        self.state_dim_len = len(self.env.observation_space.low)
+        self.state_dim_low = self.env.observation_space.low
+        self.state_dim_high = self.env.observation_space.high
 
 if __name__ == '__main__':
     simulation = Simulation()
