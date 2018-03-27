550a9511b77de489ea735affe414d4b9b3c5e94c
Sean Pollock <seanpollock3344@gmail.com>
2018-03-27 15:35:34 -0400

    hyper param and model creation'

--- Lander/Models/DeepNet.py
+++ Lander/Models/DeepNet.py
@@ -1,10 +1,23 @@
-import sys
-sys.path.append(sys.path[0] + "/..")
-
 import keras
 from keras.models import Sequential
 from keras.layers import Dense
+from keras.optimizers import Adam
 
-from .. import Simulation
+class DeepNet():
+    def __init__(self, hyper_params):
+        self.init_hyper_params(hyper_params)
 
-print(Simulation)
+    def init_hyper_params(self, params):
+        self.alpha = params['alpha']
+        self.alpha_decay = params['alpha_decay']
+        self.epsilon = params['epsilon']
+        self.epislon_min = params['epsilon_min']
+        self.epsilon_decay = params['epsilon_decay']
+
+    def build_network(self, state_dim, action_dim):
+        model = Sequential()
+        model.add(512, activation='relu', input_dim=state_dim)
+        model.add(512, activation='relu')
+        model.add(Dense(action_dim, activation='linear'))
+        model.compile(optimizer=Adam(lr=self.alph, decay=self.alpha_decay), loss='mse')
+        return model
--- Lander/Simulation.py
+++ Lander/Simulation.py
@@ -1,15 +1,15 @@
 import gym
-import random
-from time import sleep
-from Model import Model
-import numpy as np
 
 class Simulation():
     def __init__(self, model):
-        self.model = model
+        # gym environment
         self.env = gym.make('LunarLander-v2')
         self.get_action_dim()
         self.get_space_dim()
+
+        # create model
+        self.model = model
+        self.model.build_network(self.state_dim_len)
 
     def get_action_dim(self):
         self.action_dim_len =self.env.action_space.n
@@ -18,52 +18,3 @@ class Simulation():
         self.state_dim_len = len(self.env.observation_space.low)
         self.state_dim_low = self.env.observation_space.low
         self.state_dim_high = self.env.observation_space.high
-
-    def choose_action(self):
-        if np.random.random() < self.model.epsilon:
-            return np.random.randint(self.action_dim_len)
-        else:
-            q_state = model.get_q_state()
-            return np.argmax(q_sate)
-
-    def run_simulation(self, num_episodes):
-        for i in range(num_episodes):
-            self.run_episodes()
-
-    def run_episode(self):
-        self.num_ticks = 0
-        self.total_reward = 0
-
-        self.old_state = self.env.reset()
-
-        while True:
-            done = self.run_tick()
-            if done:
-                break
-
-    def run_tick(self):
-        self.num_ticks += 1
-        sleep(0.1)
-        self.env.render()
-        return self.run_step()
-
-    def run_step(self):
-        action = self.choose_action()
-        new_state, reward, done, info = self.env.step(action)
-
-        self.model.memory.append(
-            (old_state, action, reward, new_state,
-                0.0 if done else 1.0))
-
-        if self.num_tick % self.model.update_slow_target_every:
-            print('update slow target op')
-
-        old_state = new_state
-
-        return done
-
-
-if __name__ == '__main__':
-    model = Model()
-    simulation = Simulation()
-    simulation.run(3)
