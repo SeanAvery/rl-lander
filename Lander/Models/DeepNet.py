3b05986b7c3fb56408a4d4de492d33d6a7bfb722
Sean Pollock <seanpollock3344@gmail.com>
2018-03-27 13:35:34 -0400

    restructuring files

---
+++ Lander/Models/DeepNet.py
@@ -0,0 +1,10 @@
+import sys
+sys.path.append(sys.path[0] + "/..")
+
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+
+from .. import Simulation
+
+print(Simulation)
