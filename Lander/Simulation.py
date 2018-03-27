c0861649cb392e5a7d77c208e170aa6ee70bf19d
Sean Pollock <seanpollock3344@gmail.com>
2018-03-27 15:52:53 -0400

    simulation sequence

--- Lander/Simulation.py
+++ Lander/Simulation.py
@@ -1,6 +1,7 @@
 import gym
 import numpy as np
 import random
+from time import sleep
 
 class Simulation():
     def __init__(self, model):
@@ -23,6 +24,33 @@ class Simulation():
 
     def choose_action(self, state):
         if np.random.rand() <= self.model.epsilon:
-            return random
+            return np.random.randint(self.action_dim_len)
         else:
-            return np.argmax(self.model.model.predic(state))
+            return np.argmax(self.model.model.predict(state))
+
+    def run_simulation(self, num_episodes):
+        for i in range(num_episodes):
+            self.run_episode()
+
+    def run_episode(self):
+        self.num_ticks = 0
+        self.total_reward = 0
+
+        self.old_state = self.env.reset()
+
+        while True:
+            done = self.run_tick()
+            if done:
+                break
+
+    def run_tick(self):
+        sleep(0.1)
+        self.num_ticks += 1
+        self.env.render()
+        return self.run_step()
+
+    def run_step(self):
+        action = self.choose_action(self.old_state)
+        new_state, reward, done, info = self.env.step(action)
+        self.old_state = new_state
+        return done
