e38e86562338b974478703a52fefa30cf6f4e53d
Sean Pollock <seanpollock3344@gmail.com>
2018-04-18 20:00:44 -0400

    adding in simple dense model

--- Lander/Models/PriorityDeepNet.py
+++ Lander/Models/PriorityDeepNet.py
@@ -1,7 +1,12 @@
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import Adam
 
 class PriorityDeepNet():
     def __init__(self, hyper_params):
         self.init_hyper_params(hyper_params)
+        self.memory = []
 
     def init_hyper_parmas(self, params):
         self.alpha = params['alpha']
@@ -12,3 +17,14 @@ class PriorityDeepNet():
         self.batch_size = params['batch_size']
         self.gamma = params['gamma']
     
+    def build_network(self, state_dim, action_dim):
+        model = Sequential()
+        model.add(Dense(512, activation='relu', input_dim=state_dim)
+        model.add(Dense(512, activation='relu'))
+        model.add(Dense(action_dim, activation='linear'))
+        model.compile(
+            optimizer=Adam(lr=self.alpha, decay=self.alpha_decay)
+            loss='mse')
+                
+        
+
