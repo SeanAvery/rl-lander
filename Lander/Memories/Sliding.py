2b9b73b7575f319e3a057fca75262bbcc8ed5fca
Sean Pollock <seanpollock3344@gmail.com>
2019-04-09 18:02:44 -0400

    fixes to memory buffer

--- Lander/Memories/Sliding.py
+++ Lander/Memories/Sliding.py
@@ -6,14 +6,22 @@ class Memory():
         self.memories = deque()
         self.max_size = max_size
 
-    is_full = lambda x: len(self.memories) > self.max_size
+    #is_full = lambda x: len(self.memories) > self.max_size
+    
+    def is_full(self):
+        print(self.max_size)
+        if len(self.memories) > self.max_size:
+            print('its full')
+            return True
+        else:
+            return False
 
     def append(self, item):
-        if is_full:
+        if self.is_full():
             self.memories.popleft()
             self.memories.append(item)
         else:
             self.memories.append(item)
 
     def sample(self, num_items):
-        return random.sample(self.memories, min(len(self.memory), num_items))
+        return random.sample(self.memories, min(len(self.memories), num_items))
